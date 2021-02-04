627f3c5caca0ac77936653f6150ffe7b1ee1ec3e
nicholaskier <nicholaskier@gmail.com>
2021-02-04 14:08:21 -0600

    user auth up and running

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -9,6 +10,7 @@ class Hike(models.Model):
     title = models.CharField(max_length=100)
     summary = models.CharField(max_length=200)
     distance = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Hike, Photo
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 import uuid
 import boto3
 
@@ -12,14 +16,17 @@ BUCKET = 'takeahike'
 def home(request):
     return render(request, 'home.html')
 
+@login_required
 def hikes_index(request):
-    hikes = Hike.objects.all()
+    hikes = Hike.objects.filter(user=request.user)
     return render(request, 'hikes/index.html', {'hikes': hikes})
 
+@login_required
 def hikes_detail(request, hike_id):
     hike = Hike.objects.get(id=hike_id)
     return render(request, 'hikes/detail.html', {'hike': hike})
 # Create your views here.
+@login_required
 def add_photo(request, hike_id):
     # photo-file will be the "name" attribute on the <input type="file">
     photo_file = request.FILES.get('photo-file', None)
@@ -40,14 +47,37 @@ def add_photo(request, hike_id):
             print('An error occurred uploading file to S3')
     return redirect('detail', hike_id=hike_id)
 
-class HikeCreate(CreateView):
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user' form object
+    # that includes the data from the browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      # This will add the user to the database
+      user = form.save()
+      # This is how we log a user in via code
+      login(request, user)
+      return redirect('index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  # A bad POST or a GET request, so render signup.html with an empty form
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)    
+
+class HikeCreate(LoginRequiredMixin, CreateView):
+    model = Hike
+    fields = ['title', 'summary', 'distance']
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class HikeUpdate(LoginRequiredMixin, UpdateView):
     model = Hike
     fields = ['title', 'summary', 'distance']
 
-class HikeUpdate(UpdateView):
-    model = Hike
-    fields = ['title', 'summary', 'distance']
-
-class HikeDelete(DeleteView):
+class HikeDelete(LoginRequiredMixin, DeleteView):
     model = Hike
     success_url = '/hikes/'
