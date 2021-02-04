c490c429cf985a99f51deeefe955906ec1f4daff
nicholaskier <nicholaskier@gmail.com>
2021-02-04 11:04:55 -0600

    hike details rendering

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('hikes/', views.hikes_index, name='index')
+    path('hikes/', views.hikes_index, name='index'),
+    path('hikes/<int:hike_id>/', views.hikes_detail, name='detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -9,4 +9,7 @@ def hikes_index(request):
     hikes = Hike.objects.all()
     return render(request, 'hikes/index.html', {'hikes': hikes})
 
+def hikes_detail(request, hike_id):
+    hike = Hike.objects.get(id=hike_id)
+    return render(request, 'hikes/detail.html', {'hike': hike})
 # Create your views here.
